4c9e214e8a95924e2a18ccdcd72c601e8bca015c
Maxim Usachev <psywalker09@gmail.com>
2026-03-09 14:03:34 +0300

    feat: login form + UI (icons, toggle password, toast, styles)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
 import '@/entities/auth/model';
-import '@/entities/products/model';
 
 export function App() {
   return <RouterProvider router={router} />;
